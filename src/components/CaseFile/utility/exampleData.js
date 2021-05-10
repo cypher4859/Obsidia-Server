e24e34d9ca91265f547a6ef55411fac707713b43
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2021-05-10 14:25:38 -0400

    created an example utility data until can get the database setup

---
+++ src/components/CaseFile/utility/exampleData.js
@@ -0,0 +1,35 @@
+exports.testData = {
+    "111": {
+      "_user": "Holden",
+      "id": "111",
+      "fullname": "James",
+      "first": "James",
+      "month": "december",
+      "socialSecurityNumber": "234-51-6789",
+      "phones": ["343-443-2112"]
+    },
+    "222": {
+      "_user": "cypher",
+      "id": "222",
+      "fullname": "Jess",
+      "last": "JessName",
+      "driversLicenseNumber": "F234190",
+      "height": "2.6m"
+    },
+    "333": {
+      "_user": "cypher",
+      "id": "333",
+      "middle": "Senawq",
+      "emails": ["sineEmail@email.com"],
+      "height": "2.1m"
+    },
+    "444": {
+      "_user": "cypher",
+      "id": "444",
+      "age": "43",
+      "piercings": ["yes:ear"],
+      "tattoos": ["Arm: celtic symbols", "Leg: spiders"],
+      "emails": ["sineEmail@email.com"],
+      "height": "2.1m"
+    }
+  }
