404e9284019fe73144507b6772111c0ff1874409
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2021-05-10 14:24:28 -0400

    Got a working graphql setup

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var graphql = require('graphql');
-const IIdentity = require('./src/components/CaseFile/types/IIdentity').iidentity
+
+const IdentityService = require('./src/components/CaseFile/service/IdentityService')
+const root = require('./src/components/CaseFile/GQLTypes/schemaTypes').root
+const types = require('./src/components/CaseFile/GQLTypes/schemaTypes').schemaTypes
 
 const PORT = 8000;
 
-var schema = new graphql.GraphQLSchema({
-  IIdentity: IIdentity
-})
+var schema = graphql.buildSchema(types)
+console.log(types)
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
+  rootValue: root,
   graphiql: true
 }));
 
