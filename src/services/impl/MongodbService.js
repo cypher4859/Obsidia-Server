b20d171c46f6c96b6a8b1828c28ef2b4301cf03a
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2021-05-16 23:03:15 -0400

    Created the mongodb service

---
+++ src/services/impl/MongodbService.js
@@ -0,0 +1,45 @@
+const { MongoClient } = require('mongodb');
+
+class MongodbService {
+  constructor () {
+    this.initializeConnection()
+  }
+
+  client = {}
+
+  initializeConnection (username = "root", password = "example", clusterUrl = "mongo:27017") {
+    const uri = `mongodb://${username}:${password}@${clusterUrl}/?poolSize=20&writeConcern=majority`
+    this.client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  }
+
+  async pingCheckDiagnostic () {
+    try {
+      await this.client.connect()
+      await this.client.db("admin").command({ ping: 1 })
+      console.log('Connected Successfully!')
+    } finally {
+      await this.client.close()
+    }
+  }
+
+  async listDatabases () {
+    try {
+      console.log('client: ', this.client)
+      await this.client.connect()
+      const databaseList = await this.client.db().admin().listDatabases()
+      databaseList.forEach((database) => {
+        console.log(database)
+      });
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  dropDatabase () {}
+}
+
+mongoService = new MongodbService
+exports.object = mongoService
