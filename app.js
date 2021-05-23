f7bc540cb3d06f2daccc5f16b87278950744e487
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2021-05-22 21:43:40 -0400

    added the bodyParser so we can handle POSTs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var graphql = require('graphql');
+const bodyParser = require('body-parser');
 
 const mongodbService = require('./src/services/impl/MongodbService').object
 const root = require('./src/components/CaseFile/GQLTypes/schemaTypes').root
@@ -12,6 +13,8 @@ var schema = graphql.buildSchema(types)
 console.log(types)
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
